6fe5802b06ac032d7717ec6db6df27ccc333fe37
Alex K <contact_allex@yahoo.com>
2019-03-31 00:43:42 +0200

    Now preloading images to avoid weird flickerings in the game

--- src/App.vue
+++ src/App.vue
@@ -46,8 +46,32 @@ export default {
         { name: "Game", route: "/game", icon: "games" },
         { name: "Chords", route: "/chords", icon: "view_list" },
         { name: "Songs", route: "/songs", icon: "library_music" }
+      ],
+      preload: [
+        "https://i.ibb.co/zrnqq9Q/Gm.png",
+        "https://i.ibb.co/3yJjFYy/G.png",
+        "https://i.ibb.co/NCDzxSw/Fm.png",
+        "https://i.ibb.co/jvyRj1R/F.png",
+        "https://i.ibb.co/fFM8p3k/Em.png",
+        "https://i.ibb.co/h1BfcXY/E.png",
+        "https://i.ibb.co/chsHTRV/Dm.png",
+        "https://i.ibb.co/jWcKHQk/D.png",
+        "https://i.ibb.co/FhYFm8s/Cm.png",
+        "https://i.ibb.co/C7ZSDFh/C.png",
+        "https://i.ibb.co/2dV0gcV/Bm.png",
+        "https://i.ibb.co/N9HGx5n/B.png",
+        "https://i.ibb.co/kXgYxNt/A.png",
+        "https://i.ibb.co/kJCJgvX/Am.png"
       ]
     };
+  },
+  mounted() {
+    // Preload chord images
+    var chords = [];
+    for (let i = 0; i < this.preload.length; i++) {
+      chords[i] = new Image();
+      chords[i].src = this.preload[i];
+    }
   }
 };
 </script>
